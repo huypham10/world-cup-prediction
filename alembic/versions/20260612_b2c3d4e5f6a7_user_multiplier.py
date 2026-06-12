01608da397198ba9b591c3ecfb3d33e6c7a3c115
Huy Pham <q.huy.pham10@gmail.com>
2026-06-12 16:21:21 +0100

    user multiplier migration

---
+++ alembic/versions/20260612_b2c3d4e5f6a7_user_multiplier.py
@@ -0,0 +1,25 @@
+"""user_multiplier
+
+Revision ID: b2c3d4e5f6a7
+Revises: a1b2c3d4e5f6
+Create Date: 2026-06-12 00:00:00.000000+00:00
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+revision = 'b2c3d4e5f6a7'
+down_revision = 'a1b2c3d4e5f6'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column(
+        "users",
+        sa.Column("multiplier", sa.Numeric(5, 2), server_default="1", nullable=False),
+    )
+
+
+def downgrade() -> None:
+    op.drop_column("users", "multiplier")
